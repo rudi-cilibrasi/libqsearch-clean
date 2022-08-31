9d80c662025170f09b13c2b7f6a22b27c3ab63b6
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-08-31 10:15:11 -0700

    Bare-bones matrix class
    Replaces gsl_matrix. Storage only, no matrix math.

---
+++ SimpleMatrix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "SimpleMatrix.hpp"
+
+template<class T> inline void SimpleMatrix<T>::resize(const unsigned int &xdim, const unsigned int &ydim)
+{
+    m.resize(xdim);
+    for(auto v : m) v.resize(ydim);
+}
+
+template<class T> inline std::__1::vector<T> SimpleMatrix<T>::operator[](const unsigned int &i) const
+{ 
+    //std::cout << m.size() << "\n";
+    assert(i<m.size());
+    return m[i];
+}
+
+template<class T> inline std::__1::vector<T> &SimpleMatrix<T>::operator[](const unsigned int &i)
+{ 
+    assert(i<m.size());
+    return m[i];
+}
+
+// test for SimpleMatrix - used as main() in initial testing
+void testSimpleMatrix() {
+    SimpleMatrix< unsigned int> a(3,4);
+    
+    a[2][3] = 1;
+    std::cout << a[2][3] << "\n";
+    a.resize(4,5);
+    std::cout << a[2][3] << "\n";
+}
+
+// for stand-alone test
+// int main() { testSimpleMatrix(); }
+
+template class SimpleMatrix<unsigned int>;
