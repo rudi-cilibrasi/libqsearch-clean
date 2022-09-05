71e28702091d0f7bcb751e085f8a8cf4e42dfa5a
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-09-05 16:00:13 -0700

    Modify parameters, defer or deprecate functions

--- src/QSearchTree.hpp
+++ src/QSearchTree.hpp
@@ -4,8 +4,6 @@
 #include <vector>
 #include "SimpleMatrix.hpp"
 
-struct LabeledMatrix {};
-
 #define CHUNKSIZE 1
 
 #define NODE_FLAG_FRINGE       0x01
@@ -32,7 +30,7 @@ typedef enum {
   NODE_TYPE_ALL
 } node_type;
 
-//typedef std::unique_ptr< QSearchNeighborList > nptr;
+typedef std::vector< unsigned int > node_list;
 
 struct QSearchTree {
   int total_node_count;
@@ -44,92 +42,81 @@ struct QSearchTree {
   MutationStatistics ms;
   double score;
   std::vector< QSearchNeighborList > n;   
-  std::vector< unsigned int > p1, p2;
-  SimpleMatrix<unsigned int > spm;
+  node_list p1, p2;
+  QMatrix<unsigned int > spm;
   std::vector< unsigned int > nodeflags;
-  std::vector< unsigned int > leaf_placement;
+  node_list leaf_placement;
+  // distance matrix
+  QMatrix<double>& dm; // Using reference here as we don't want to be copying this big matrix a lot
 
   // QSearchTree(); // should default constructor exist?
-  QSearchTree(int how_many_leaves);    // is how_many_leaves needed in C++?
-  QSearchTree(QSearchTree& q);  // copy constructor replaces clone()
-  QSearchTree(QSearchTree& q, int howManyTries, const SimpleMatrix<unsigned int>& dm); // replaces qsearch_tree_find_better_tree()
+  QSearchTree(QMatrix<double>& dm_init);    // is how_many_leaves needed in C++?
+  //QSearchTree(QSearchTree& q) : dm( q.dm );  // copy constructor replaces clone()
+  QSearchTree(QSearchTree& q, int howManyTries); // replaces qsearch_tree_find_better_tree()
   // qsearch_tree_free() would become ~QSearchTree()
   // however implicit destructor is all that is needed as C++ objects clean their own memory
   //~QSearchTree();
 
+  //    std::string to_s(); // deprecated - not called 
+
   // function declared but not implemented in C - deprecated
   // bool is_leaf_node(const int& whichNode);
-  void calc_min_max(const SimpleMatrix<unsigned int>& dm);
+  void calc_min_max();
   unsigned int get_node_count();
   unsigned int get_leaf_node_count();
   unsigned int get_kernel_node_count();
-  SimpleMatrix<unsigned int> get_adjacency_matrix();
+  QMatrix<unsigned int> get_adjacency_matrix();
   bool is_connected(const unsigned int& a, const unsigned int& b);
   bool is_standard_tree();
   unsigned int get_neighbor_count(const unsigned int& a);
   void connect(const unsigned int& a, const unsigned int& b);
   void disconnect(const unsigned int& a, const unsigned int& b);
-  void find_path(std::vector<unsigned int>& result, unsigned int a, unsigned int b);
-  void find_path_fast(std::vector<unsigned int>& result, unsigned int a, unsigned int b);
+  void find_path(node_list& result, unsigned int a, unsigned int b);
+  void find_path_fast(node_list& result, unsigned int a, unsigned int b);
+  unsigned int find_path_length(unsigned int& a, unsigned int& b);
   void freshen_spm();
-  bool is_consistent_quartet(const int& a, const int& b, const int& c, const int& d);
-
-  int get_random_node(const int& what_kind);
-  int get_random_node_but_not(const int& what_kind, const int& but_not);
-  int find_path_length(const int& a, const int& b);
-  int get_random_neighbor(const int& who);
-  // change to call by reference
-  std::vector<unsigned int>& get_neighbors(const int& who);
+  bool is_consistent_quartet(unsigned int& a, unsigned int& b, unsigned int& c, unsigned int& d);
+  unsigned int get_random_node(const node_type& what_kind);
+  unsigned int get_random_node_but_not(const node_type& what_kind, const unsigned int& but_not);
+  unsigned int get_random_neighbor(const unsigned int& who);
+  // obsolete?
+    void get_neighbors(node_list& neighbors, const unsigned int& who); // changed to call by reference
   bool is_valid_tree();
   void complex_mutation();
+  int get_mutation_distribution_sample();
   void simple_mutation();
   void simple_mutation_leaf_swap();
   void simple_mutation_subtree_transfer();
   void simple_mutation_subtree_interchange();
-  void simple_mutation();
   bool can_subtree_transfer();
   bool can_subtree_interchange();
-  // change to call by reference
-  std::vector<unsigned int>& walk_tree(const int& fromwhere, bool f_bfs);
-  std::vector<unsigned int>& walk_tree_bfs(const int& fromwhere);
-  std::vector<unsigned int>& walk_tree_dfs(const int& fromwhere);
-  double calculate_order_cost(const SimpleMatrix<unsigned int> *dm);
-  int get_column_number(const int& nodenum);
-  int get_node_number(const int& columnnum);
+  // changed to call by reference - use iterator?
+  void walk_tree(node_list& result, const unsigned int& fromwhere, bool f_bfs);  
+  void walk_tree_bfs(node_list& result, const unsigned int& fromwhere);
+  void walk_tree_dfs(node_list& result, const unsigned int& fromwhere);
+  double calculate_order_cost();
+  unsigned int get_column_number(const unsigned int& nodenum);
+  // unsigned int get_node_number(const int& columnnum); // deprecated - not called
   void mutate_order_simple();
   void mutate_order_complex();
-  std::string to_s();
-  // change to call by reference
-  std::vector<unsigned int>& flipped_node_order();
-  std::vector<unsigned int>& walk_filtered(const int& fromwhere, const int& filterType);
-
-  /* Returns true if every node has exactly 1 or 3 neighbors */
-  bool is_tree_ternary();
-
-  /* Sets the "connectedness" state (TRUE or FALSE) between nodes a and b and
-  * returns the old connectedness status that was overwritten.
-  */
-  bool set_connected(const int& a, const int& b, bool newconstate);
-
+  void flipped_node_order(node_list& nodes);  // changed to call by reference
+  void walk_filtered(node_list& nodes, const int& fromwhere, const int& filterType);
+  bool is_tree_ternary(); // Returns true if every node has exactly 1 or 3 neighbors
+  // Sets the "connectedness" state (TRUE or FALSE) between nodes a and b and
+  // returns the old connectedness status that was overwritten.
+  bool set_connected(const unsigned int& a, const unsigned int& b, bool newconstate);
   void clear_all_connections();
-  double read_from_dot(std::string treedot, LabeledMatrix& lm);
+  // deferred. Is dm needed? Could this be constructor?
+    double read_from_dot(std::string treedot, QMatrix<unsigned int>& dm);  
+  double score_tree();
+  // double score_tree_fast(); - deprecated - not called
+  double score_tree_fast_v2();
 
-  // are these functions called?
-  double score_tree(const SimpleMatrix<unsigned int>& dm);
+  // from QLabeledTree - deferred
+    std::string to_dot();
+    std::string to_nexus();
+    std::string to_nexus_full(QMatrix< unsigned int >& dm); // could be an overload of to_nexus()
 
-};
-
-class QLabeledTree {
-  QSearchTree& qs;
-  std::vector<std::string> labels;
-  SimpleMatrix<unsigned int> mat;
-
-  QLabeledTree(const QSearchTree& tree, const LabeledMatrix& lm); // replaces qsearch_tree_add_labels()
-  ~QLabeledTree(); 
-
-  std::string to_dot();
-  std::string to_nexus();
-  std::string to_nexus_full(LabeledMatrix& lm); // could be an overload of to_nexus()
 };
 
 #endif // __QSEARCHTREE_HPP
