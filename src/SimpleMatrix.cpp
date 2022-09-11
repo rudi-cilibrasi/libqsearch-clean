2aab37a3f601667ad841edf44cf4dd08f8ac8952
Joy Hughes <goddessjoy@gmail.com>
2022-09-11 14:32:52 -0700

    fix QMatrix functions

--- src/SimpleMatrix.cpp
+++ src/SimpleMatrix.cpp
@@ -25,10 +25,10 @@ void print_string_list( const StringList& v, const std::string spacer )
 template<class T> inline void QMatrix<T>::resize(const unsigned int &dim)
 { m.resize(dim); for(auto v : m) v.resize(dim); }
 
-template<class T> inline std::__1::vector<T> QMatrix<T>::operator[](const unsigned int &i) const
+template<class T> inline std::vector<T> QMatrix<T>::operator[](const unsigned int &i) const
 { assert(i<m.size()); return m[i]; }
 
-template<class T> inline std::__1::vector<T> &QMatrix<T>::operator[](const unsigned int &i)
+template<class T> inline std::vector<T> &QMatrix<T>::operator[](const unsigned int &i)
 { assert(i<m.size()); return m[i]; }
 
 template<class T> bool QMatrix<T>::has_labels()
