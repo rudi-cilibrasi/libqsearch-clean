ab12db7a3abfc06621b0b10513b1847820b2dd1f
Joy Hughes <goddessjoy@gmail.com>
2022-09-11 14:25:29 -0700

    fix unit test

--- src/SimpleMatrix.cpp
+++ src/SimpleMatrix.cpp
@@ -93,27 +93,5 @@ template<class T> void QMatrix<T>::from_string( const std::string& s )
     assert( ( labels.size() == 0 ) || ( labels.size() == dim ) );
 }
 
-// test for QMatrix - used in main() in initial testing
-void testQMatrix() {
-    QMatrix< unsigned int> q;
-    /*
-    for( int i=0; i<q.dim; i++ ) {
-        for( int j=0; j<q.dim; j++ ) {
-            q[i][j] = i*j;
-        }
-    } 
-    q.labels = { "zero", "one", "two", "three" };
-    */
-    std::string input = "zero 0 0 0 0\none 0 1 2 3\ntwo 0 2 4 6\nthree 0 3 6 9\n";
-    q.from_string( input );
-    std::string s;  
-    q.to_string(s);
-    std::cout << "\nString produced by matrix\n";
-    std::cout << s;
-}
-
-// for stand-alone test
-int main() { testQMatrix(); return 0; }
-
 template class QMatrix<unsigned int>;
 template class QMatrix<double>;
