7c2a98adf1d9cb9658953b08845c3b8692c6a3ec
Joy Hughes <goddessjoy@gmail.com>
2022-09-11 14:26:53 -0700

    fix unit test (2)

--- src/test-qsearch.cpp
+++ src/test-qsearch.cpp
@@ -1,14 +1,22 @@
 #include <SimpleMatrix.hpp>
 
-// test for QMatrix - used as main() in initial testing
-void testQMatrix()
-{
-  QMatrix<unsigned int> a(4);
-
-  a[2][3] = 1;
-  std::cout << a[2][3] << "\n";
-  a.resize(5);
-  std::cout << a[2][3] << "\n";
+// test for QMatrix - used in main() in initial testing
+void testQMatrix() {
+    QMatrix< unsigned int> q;
+    /*
+    for( int i=0; i<q.dim; i++ ) {
+        for( int j=0; j<q.dim; j++ ) {
+            q[i][j] = i*j;
+        }
+    } 
+    q.labels = { "zero", "one", "two", "three" };
+    */
+    std::string input = "zero 0 0 0 0\none 0 1 2 3\ntwo 0 2 4 6\nthree 0 3 6 9\n";
+    q.from_string( input );
+    std::string s;  
+    q.to_string(s);
+    std::cout << "\nString produced by matrix\n";
+    std::cout << s;
 }
 
 // for stand-alone test
