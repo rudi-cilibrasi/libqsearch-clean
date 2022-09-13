682e694577268a8e79b09f5f07371695ff6e675b
Joy Hughes <goddessjoy@gmail.com>
2022-09-13 10:44:04 -0700

    String processing and I/O

---
+++ src/StringTools.hpp
@@ -0,0 +1,18 @@
+#ifndef __STRING_TOOLS_HPP
+#define __STRING_TOOLS_HPP
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+typedef std::vector<std::string> StringList;
+
+// Breaks a string up into a vector of substrings wherever separated by a given character
+void segment_string( StringList& v, const std::string& s, const unsigned char c );
+void print_string_list( const StringList& v, const std::string spacer );
+bool read_whole_file( std::string& s, const std::string& filename );
+bool write_whole_file( const std::string& s, const std::string& filename );
+
+#endif // __STRING_TOOLS_HPP
