f6f5e8ab793d65f66f263e09721ba18ed498a4a6
Nam V. Do <vannam12a7@gmail.com>
2024-11-27 09:02:05 +0700

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,1 @@
-sudo apt-get update
-sudo apt-get install -y docker.io docker-compose
-
-# Start Docker service
-sudo systemctl start docker
-sudo systemctl enable docker
-
-sudo docker-compose up -d --build
+docker-compose --profile redis --profile app up -d
